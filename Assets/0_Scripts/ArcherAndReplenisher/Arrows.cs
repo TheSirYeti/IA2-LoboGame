606cea6a2b0707673f3c2bfd03bfcf01d1c5f22d
thieflin <ignaciogarrido1234@hotmail.com>
2023-07-25 21:42:33 -0300

    archerrr

---
+++ Assets/0_Scripts/ArcherAndReplenisher/Arrows.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrows : MonoBehaviour
+{
+    private Rigidbody _rb;
+    [SerializeField] float _arrowSpeed;
+    [SerializeField] Vector3 _dir;
+    float dirX, dirY, dirZ;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+    void Start()
+    {
+        dirX = Random.Range(-.9f, .9f);
+        dirY = Random.Range(.5f, 1.2f);
+        dirZ = Random.Range(1f, 3f);
+        _dir = new Vector3(dirX, dirY, dirZ);
+        _rb.AddForce(_dir * _arrowSpeed, ForceMode.Acceleration);
+    }
+}
