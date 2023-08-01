640f1bf3287f1d562a60a4dac484cc098f12090e
JP Rshaid <jprshaid123@gmail.com>
2023-08-01 19:42:22 -0300

    Damage for Arrows

--- Assets/0_Scripts/ArcherAndReplenisher/Arrows.cs
+++ Assets/0_Scripts/ArcherAndReplenisher/Arrows.cs
@@ -1,11 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Arrows : MonoBehaviour
 {
     private Rigidbody _rb;
     [SerializeField] float _arrowSpeed;
+    [SerializeField] float _damage;
     [SerializeField] Vector3 _dir;
     float dirX, dirY, dirZ;
 
@@ -13,13 +16,24 @@ public class Arrows : MonoBehaviour
     {
         _rb = GetComponent<Rigidbody>();
     }
+    
     void Start()
     {
-        dirX = Random.Range(-.4f, .4f);
+        dirX = Random.Range(-.2f, .2f);
         dirY = Random.Range(.5f, 1f);
-        dirZ = Random.Range(1f, 3f);
+        dirZ = Random.Range(0.5f, 1.5f);
         _dir = new Vector3(dirX, dirY, dirZ);
         transform.forward = _dir;
         _rb.AddForce(_dir * _arrowSpeed, ForceMode.Acceleration);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var entity = other.GetComponent<IEntity>();
+
+        if (entity != null && entity.IsEnemy)
+        {
+            entity.TakeDamage(_damage);
+        }
     }
 }
